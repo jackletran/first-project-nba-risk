d8bb503d6e28fb4a8ff23408a9aa62d3c68a1c4c
jackletran <jack.letran@moebel.de>
2022-06-28 13:54:48 +0200

    fix switch case for coloring countries

--- test.js
+++ test.js
@@ -35,6 +35,9 @@ const stephCurry = {
       IN: 0,
       OH: 0,
       AL: 0,
+      KY: 0,
+      MS: 0,
+      TN: 0,
     },
   },
 };
@@ -76,6 +79,9 @@ const kyrieIrving = {
       IN: 0,
       OH: 0,
       AL: 0,
+      KY: 0,
+      MS: 0,
+      TN: 0,
     },
   },
 };
@@ -232,7 +238,6 @@ function attachBoostEventListener() {
   const boostButtonSelect = boostButtonArray[0];
   let selectedState;
   let boostLeftover = stephCurry.boostUnits;
-  let CURRYCOUNTRY = stephCurry.stateInfo.unitsInThatState.TX; // man muss die genaue info da hinschreiben, die Variable bringt nichts
   let howManyUnitsYouHavePlaced = 0;
   const stephCurryStates = Object.keys(stephCurry.stateInfo.unitsInThatState);
   const stephCurryUnitsInThatState = Object.values(
@@ -256,353 +261,15 @@ function attachBoostEventListener() {
     if (boostLeftover > 0) {
       boostLeftover--;
       selectedState = event.target.id;
-      // for (let i = 0; i < stephCurryStates.length; i++) {
-      //   if (stephCurryStates[i].includes(selectedState)) {
-      //     stephCurryUnitsInThatState++;
-      //     //
-      //     let stateText = document.getElementById(
-      //       `${stephCurryStates[i]}-text`
-      //     );
-      //     stateText.innerHTML = `${stephCurryUnitsInThatState[i]}`;
-      //     // stephCurry.stateInfo.unitsInThatState.TX = .stephCurryUnitsInThatState;
-      //     // OR
-      //     // stephCurry.stateInfo.unitsInThatState.TX++;
-      //   }
-      // }
-      switch (true) {
-        case selectedState === "AL":
-          stephCurry.stateInfo.unitsInThatState.AL++;
-          break;
-        case selectedState === "AK":
-          stephCurry.stateInfo.unitsInThatState.AK++;
-          break;
-        case selectedState === "AZ":
-          stephCurry.stateInfo.unitsInThatState.AZ++;
-          break;
-        case selectedState === "AR":
-          stephCurry.stateInfo.unitsInThatState.AR++;
-          break;
-        case selectedState === "CA":
-          stephCurry.stateInfo.unitsInThatState.CA++;
-          break;
-        case selectedState === "CZ":
-          stephCurry.stateInfo.unitsInThatState.CZ++;
-          break;
-        case selectedState === "CO":
-          stephCurry.stateInfo.unitsInThatState.CO++;
-          break;
-        case selectedState === "CT":
-          stephCurry.stateInfo.unitsInThatState.CT++;
-          break;
-        case selectedState === "DE":
-          stephCurry.stateInfo.unitsInThatState.DE++;
-          break;
-        case selectedState === "DC":
-          stephCurry.stateInfo.unitsInThatState.DC++;
-          break;
-        case selectedState === "FL":
-          stephCurry.stateInfo.unitsInThatState.FL++;
-          break;
-        case selectedState === "GA":
-          stephCurry.stateInfo.unitsInThatState.GA++;
-          break;
-        case selectedState === "GU":
-          stephCurry.stateInfo.unitsInThatState.GU++;
-          break;
-        case selectedState === "HI":
-          stephCurry.stateInfo.unitsInThatState.HI++;
-          break;
-        case selectedState === "ID":
-          stephCurry.stateInfo.unitsInThatState.ID++;
-          break;
-        case selectedState === "IL":
-          stephCurry.stateInfo.unitsInThatState.IL++;
-          break;
-        case selectedState === "IN":
-          stephCurry.stateInfo.unitsInThatState.IN++;
-          break;
-        case selectedState === "IA":
-          stephCurry.stateInfo.unitsInThatState.IA++;
-          break;
-        case selectedState === "KS":
-          stephCurry.stateInfo.unitsInThatState.KS++;
-          break;
-        case selectedState === "KY":
-          stephCurry.stateInfo.unitsInThatState.KY++;
-          break;
-        case selectedState === "LA":
-          stephCurry.stateInfo.unitsInThatState.LA++;
-          break;
-        case selectedState === "ME":
-          stephCurry.stateInfo.unitsInThatState.ME++;
-          break;
-        case selectedState === "MD":
-          stephCurry.stateInfo.unitsInThatState.MD++;
-          break;
-        case selectedState === "MA":
-          stephCurry.stateInfo.unitsInThatState.MA++;
-          break;
-        case selectedState === "MI":
-          stephCurry.stateInfo.unitsInThatState.MI++;
-          break;
-        case selectedState === "MN":
-          stephCurry.stateInfo.unitsInThatState.MN++;
-          break;
-        case selectedState === "MS":
-          stephCurry.stateInfo.unitsInThatState.MS++;
-          break;
-        case selectedState === "MO":
-          stephCurry.stateInfo.unitsInThatState.MO++;
-          break;
-        case selectedState === "MT":
-          stephCurry.stateInfo.unitsInThatState.MT++;
-          break;
-        case selectedState === "NE":
-          stephCurry.stateInfo.unitsInThatState.NE++;
-          break;
-        case selectedState === "NV":
-          stephCurry.stateInfo.unitsInThatState.NV++;
-          break;
-        case selectedState === "NH":
-          stephCurry.stateInfo.unitsInThatState.NH++;
-          break;
-        case selectedState === "NJ":
-          stephCurry.stateInfo.unitsInThatState.NJ++;
-          break;
-        case selectedState === "NM":
-          stephCurry.stateInfo.unitsInThatState.NM++;
-          break;
-        case selectedState === "NY":
-          stephCurry.stateInfo.unitsInThatState.NY++;
-          break;
-        case selectedState === "NC":
-          stephCurry.stateInfo.unitsInThatState.NC++;
-          break;
-        case selectedState === "ND":
-          stephCurry.stateInfo.unitsInThatState.ND++;
-          break;
-        case selectedState === "OH":
-          stephCurry.stateInfo.unitsInThatState.OH++;
-          break;
-        case selectedState === "OK":
-          stephCurry.stateInfo.unitsInThatState.OK++;
-          break;
-        case selectedState === "OR":
-          stephCurry.stateInfo.unitsInThatState.OR++;
-          break;
-        case selectedState === "PA":
-          stephCurry.stateInfo.unitsInThatState.PA++;
-          break;
-        case selectedState === "PR":
-          stephCurry.stateInfo.unitsInThatState.PR++;
-          break;
-        case selectedState === "RI":
-          stephCurry.stateInfo.unitsInThatState.RI++;
-          break;
-        case selectedState === "SC":
-          stephCurry.stateInfo.unitsInThatState.SC++;
-          break;
-        case selectedState === "SD":
-          stephCurry.stateInfo.unitsInThatState.SD++;
-          break;
-        case selectedState === "TN":
-          stephCurry.stateInfo.unitsInThatState.TN++;
-          break;
-        case selectedState === "TX":
-          stephCurry.stateInfo.unitsInThatState.TX++;
-          break;
-        case selectedState === "UT":
-          stephCurry.stateInfo.unitsInThatState.UT++;
-          break;
-        case selectedState === "VT":
-          stephCurry.stateInfo.unitsInThatState.VT++;
-          break;
-        case selectedState === "VI":
-          stephCurry.stateInfo.unitsInThatState.VI++;
-          break;
-        case selectedState === "VA":
-          stephCurry.stateInfo.unitsInThatState.VA++;
-          break;
-        case selectedState === "WA":
-          stephCurry.stateInfo.unitsInThatState.WA++;
-          break;
-        case selectedState === "WV":
-          stephCurry.stateInfo.unitsInThatState.WV++;
-          break;
-        case selectedState === "WI":
-          stephCurry.stateInfo.unitsInThatState.WI++;
-          break;
-        case selectedState === "WY":
-          stephCurry.stateInfo.unitsInThatState.WY++;
-          break;
-        // Wenn Spieler 2 dran ist
-        // case selectedState === "AL":
-        //   kyrieIrving.stateInfo.unitsInThatState.AL++;
-        //   break;
-        // case selectedState === "AK":
-        //   kyrieIrving.stateInfo.unitsInThatState.AK++;
-        //   break;
-        // case selectedState === "AZ":
-        //   kyrieIrving.stateInfo.unitsInThatState.AZ++;
-        //   break;
-        // case selectedState === "AR":
-        //   kyrieIrving.stateInfo.unitsInThatState.AR++;
-        //   break;
-        // case selectedState === "CA":
-        //   kyrieIrving.stateInfo.unitsInThatState.CA++;
-        //   break;
-        // case selectedState === "CZ":
-        //   kyrieIrving.stateInfo.unitsInThatState.CZ++;
-        //   break;
-        // case selectedState === "CO":
-        //   kyrieIrving.stateInfo.unitsInThatState.CO++;
-        //   break;
-        // case selectedState === "CT":
-        //   kyrieIrving.stateInfo.unitsInThatState.CT++;
-        //   break;
-        // case selectedState === "DE":
-        //   kyrieIrving.stateInfo.unitsInThatState.DE++;
-        //   break;
-        // case selectedState === "DC":
-        //   kyrieIrving.stateInfo.unitsInThatState.DC++;
-        //   break;
-        // case selectedState === "FL":
-        //   kyrieIrving.stateInfo.unitsInThatState.FL++;
-        //   break;
-        // case selectedState === "GA":
-        //   kyrieIrving.stateInfo.unitsInThatState.GA++;
-        //   break;
-        // case selectedState === "GU":
-        //   kyrieIrving.stateInfo.unitsInThatState.GU++;
-        //   break;
-        // case selectedState === "HI":
-        //   kyrieIrving.stateInfo.unitsInThatState.HI++;
-        //   break;
-        // case selectedState === "ID":
-        //   kyrieIrving.stateInfo.unitsInThatState.ID++;
-        //   break;
-        // case selectedState === "IL":
-        //   kyrieIrving.stateInfo.unitsInThatState.IL++;
-        //   break;
-        // case selectedState === "IN":
-        //   kyrieIrving.stateInfo.unitsInThatState.IN++;
-        //   break;
-        // case selectedState === "IA":
-        //   kyrieIrving.stateInfo.unitsInThatState.IA++;
-        //   break;
-        // case selectedState === "KS":
-        //   kyrieIrving.stateInfo.unitsInThatState.KS++;
-        //   break;
-        // case selectedState === "KY":
-        //   kyrieIrving.stateInfo.unitsInThatState.KY++;
-        //   break;
-        // case selectedState === "LA":
-        //   kyrieIrving.stateInfo.unitsInThatState.LA++;
-        //   break;
-        // case selectedState === "ME":
-        //   kyrieIrving.stateInfo.unitsInThatState.ME++;
-        //   break;
-        // case selectedState === "MD":
-        //   kyrieIrving.stateInfo.unitsInThatState.MD++;
-        //   break;
-        // case selectedState === "MA":
-        //   kyrieIrving.stateInfo.unitsInThatState.MA++;
-        //   break;
-        // case selectedState === "MI":
-        //   kyrieIrving.stateInfo.unitsInThatState.MI++;
-        //   break;
-        // case selectedState === "MN":
-        //   kyrieIrving.stateInfo.unitsInThatState.MN++;
-        //   break;
-        // case selectedState === "MS":
-        //   kyrieIrving.stateInfo.unitsInThatState.MS++;
-        //   break;
-        // case selectedState === "MO":
-        //   kyrieIrving.stateInfo.unitsInThatState.MO++;
-        //   break;
-        // case selectedState === "MT":
-        //   kyrieIrving.stateInfo.unitsInThatState.MT++;
-        //   break;
-        // case selectedState === "NE":
-        //   kyrieIrving.stateInfo.unitsInThatState.NE++;
-        //   break;
-        // case selectedState === "NV":
-        //   kyrieIrving.stateInfo.unitsInThatState.NV++;
-        //   break;
-        // case selectedState === "NH":
-        //   kyrieIrving.stateInfo.unitsInThatState.NH++;
-        //   break;
-        // case selectedState === "NJ":
-        //   kyrieIrving.stateInfo.unitsInThatState.NJ++;
-        //   break;
-        // case selectedState === "NM":
-        //   kyrieIrving.stateInfo.unitsInThatState.NM++;
-        //   break;
-        // case selectedState === "NY":
-        //   kyrieIrving.stateInfo.unitsInThatState.NY++;
-        //   break;
-        // case selectedState === "NC":
-        //   kyrieIrving.stateInfo.unitsInThatState.NC++;
-        //   break;
-        // case selectedState === "ND":
-        //   kyrieIrving.stateInfo.unitsInThatState.ND++;
-        //   break;
-        // case selectedState === "OH":
-        //   kyrieIrving.stateInfo.unitsInThatState.OH++;
-        //   break;
-        // case selectedState === "OK":
-        //   kyrieIrving.stateInfo.unitsInThatState.OK++;
-        //   break;
-        // case selectedState === "OR":
-        //   kyrieIrving.stateInfo.unitsInThatState.OR++;
-        //   break;
-        // case selectedState === "PA":
-        //   kyrieIrving.stateInfo.unitsInThatState.PA++;
-        //   break;
-        // case selectedState === "PR":
-        //   kyrieIrving.stateInfo.unitsInThatState.PR++;
-        //   break;
-        // case selectedState === "RI":
-        //   kyrieIrving.stateInfo.unitsInThatState.RI++;
-        //   break;
-        // case selectedState === "SC":
-        //   kyrieIrving.stateInfo.unitsInThatState.SC++;
-        //   break;
-        // case selectedState === "SD":
-        //   kyrieIrving.stateInfo.unitsInThatState.SD++;
-        //   break;
-        // case selectedState === "TN":
-        //   kyrieIrving.stateInfo.unitsInThatState.TN++;
-        //   break;
-        // case selectedState === "TX":
-        //   kyrieIrving.stateInfo.unitsInThatState.TX++;
-        //   break;
-        // case selectedState === "UT":
-        //   kyrieIrving.stateInfo.unitsInThatState.UT++;
-        //   break;
-        // case selectedState === "VT":
-        //   kyrieIrving.stateInfo.unitsInThatState.VT++;
-        //   break;
-        // case selectedState === "VI":
-        //   kyrieIrving.stateInfo.unitsInThatState.VI++;
-        //   break;
-        // case selectedState === "VA":
-        //   kyrieIrving.stateInfo.unitsInThatState.VA++;
-        //   break;
-        // case selectedState === "WA":
-        //   kyrieIrving.stateInfo.unitsInThatState.WA++;
-        //   break;
-        // case selectedState === "WV":
-        //   kyrieIrving.stateInfo.unitsInThatState.WV++;
-        //   break;
-        // case selectedState === "WI":
-        //   kyrieIrving.stateInfo.unitsInThatState.WI++;
-        //   break;
-        // case selectedState === "WY":
-        //   kyrieIrving.stateInfo.unitsInThatState.WY++;
-        //   break;
-        default:
-          console.log("Something is wrong!");
+      for (let i = 0; i < stephCurryStates.length; i++) {
+        if (stephCurryStates[i].includes(selectedState)) {
+          let stateText = document.getElementById(
+            `${stephCurryStates[i]}-text`
+          );
+          stephCurry.stateInfo.unitsInThatState[`${selectedState}`]++;
+          stateText.innerHTML =
+            stephCurry.stateInfo.unitsInThatState[`${selectedState}`];
+        }
       }
       howManyUnitsYouHavePlaced++;
       getInnerResultBox.innerHTML = `You placed ${howManyUnitsYouHavePlaced} units on ${selectedState}. You have ${boostLeftover} units left.`;
